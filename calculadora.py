e3581175452372fd6a9b49b9e4c5388520abac44
Luiz Balestrassi <luiz.balestrassi@gmail.com>
2026-08-19 17:17:32 -0300

    Adiciona comentario

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,10 @@
 print("Bem-vindo à Calculadora em Python!")
- 
+
+ #Recebe Valores
 numero1 = float(input("Digite o primeiro número: "))
 numero2 = float(input("Digite o segundo número: "))
 
+ #Recebe Operador
 print("Menu de opções")
 print("1. Soma")
 print("2. Subtração")
@@ -10,15 +12,20 @@ print("3. Multiplicação")
 print("4. Divisão")
 opcao = input("Digite a operação desejada: ")
 
+ #Realiza operação e mostra resultado
 if opcao == "1":
     resultado = numero1 + numero2
+    print("O resultado é:", resultado)
 elif opcao == "2":
     resultado = numero1 - numero2
+    print("O resultado é:", resultado)
 elif opcao == "3":
     resultado = numero1 * numero2
+    print("O resultado é:", resultado)
 elif opcao == "4": 
     if numero2 != 0:
         resultado = numero1 / numero2
+        print("O resultado é:", resultado)
     else:
         print("Não é possível dividir por zero!")
 else:
